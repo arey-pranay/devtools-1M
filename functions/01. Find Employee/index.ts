9f90f6d6d9605a1787d12d72825664ab16b1aa61
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2026-05-13 12:56:21 +0530

    Update index.ts

--- functions/01. Find Employee/index.ts
+++ functions/01. Find Employee/index.ts
@@ -8,17 +8,28 @@ function findEmployee(
   employees: Employee | null,
   id: number
 ): Employee | null {
-  if(!employees) return null;
-    if(employees.id==id){
-      return employees;
-    } 
 
-    if(employees.children){
-      for(const emp of employees.children){
-        const temp = findEmployee(emp,id);
-        if(temp) return temp;
-      }
+  // Base case:
+  // If employee node doesn't exist, target isn't found
+  if (!employees) return null;
+
+  // Check current employee
+  if (employees.id === id) {
+    return employees;
+  }
+
+  // Recursively search all child employees
+  if (employees.children) {
+    for (const emp of employees.children) {
+
+      // DFS search inside subtree
+      const temp = findEmployee(emp, id);
+
+      // If employee found in subtree, return immediately
+      if (temp) return temp;
     }
+  }
 
+  // Employee not found in current subtree
   return null;
 }
